bfaa4d9eb6a1ebd0dbe1688b8c06cd30fafa3f23
hj <gudwndhkd@nate.com>
2019-05-23 21:31:02 +0900

    input 검색 필터링 예제 추가. 리랜더링 방지 코드 추가

--- src/App.js
+++ src/App.js
@@ -18,7 +18,13 @@ class App extends Component {
         name: '사람',
         phone: '010-0000-0001'
       },
-    ]
+    ],
+    keyword: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
   }
   handleCreate = (data) => {
     const { information } = this.state;
@@ -45,14 +51,23 @@ class App extends Component {
   }
 
   render() {
-    const { information } = this.state;
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        <p><input
+          placeholder="검색할 이름을 입력하세요"
+          onChange={this.handleChange}
+          value={keyword}
+        />
+        </p>
         <PhoneInfoList 
-          data={information}
+          data={filteredList}
           onRemove={this.handleRemove}
           onUpdate={this.handleUpdate}
         />
--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -42,7 +42,6 @@ class PhoneInfo extends Component {
   // 여기서는, editing 값이 바뀔 때 처리 할 로직이 적혀있습니다.
   // 수정을 눌렀을땐, 기존의 값이 input에 나타나고,
   // 수정을 적용할땐, input 의 값들을 부모한테 전달해줍니다.
-
   const {info, onUpdate} = this.props;
     if(!prevState.editing && this.state.editing){
       // editing 값이 false -> true 로 전환 될 때
@@ -63,7 +62,19 @@ class PhoneInfo extends Component {
 
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // 수정 상태가 아니고, info 값이 같다면 리렌더링 안함
+    if (!this.state.editing  
+        && !nextState.editing
+        && nextProps.info === this.props.info) {
+      return false;
+    }
+    // 나머지 경우엔 리렌더링함
+    return true;
+  }
+
   render(){
+    console.log('render PhoneInfo ' + this.props.info.id);
     const style = {
       border: '1px solid black',
       padding: '8px',
